40bb5fa3ff8eba2f04e9735d26fac6d70a577cce
houssamch777 <84366953+houssamch777@users.noreply.github.com>
2023-05-25 19:52:22 +0100

    Update example_d3.py

--- Python-Neural-Network-houssem-eddine-cheriet/example_d3.py
+++ Python-Neural-Network-houssem-eddine-cheriet/example_d3.py
@@ -41,33 +41,22 @@ net.add(FCLayer(2, 32))
 net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(32, 16))
 net.add(ActivationLayer(tanh, tanh_prime))
-
-
 net.add(FCLayer(16, 1))
 net.add(ActivationLayer(tanh, tanh_prime))
-
 # Train
 net.use(mse, mse_prime)
-net.fit(x_train, y_train, epochs=2000, learning_rate=0.01)
-
+net.fit(x_train, y_train, epochs=5000, learning_rate=0.01)
 # Test
 xt3_reshaped = np.array([xt3[:, i].reshape(1, 2) for i in range(ntest)])  # Reshape xt3 to (1, 2, ntest)
-
-
-
 yt3_reshaped = np.array([yt3[i].reshape(1, 1) for i in range(napp)])  # Reshape xt1 to (1, 2, ntest)
 # After training the network
 accuracy = net.accuracy(xt3_reshaped, yt3_reshaped)
 print('Accuracy: %.2f' % accuracy)
-
-
 out = np.array(net.predict(xt3_reshaped)).flatten()  # Flatten the output array
-
 # Use only the first ntest samples for plotting
 xt3_plot = xt3[:, :ntest]
 yt3_plot = yt3[:ntest]
 out_plot = out[:ntest]
-
 plt.figure(figsize=(8, 6))
 plt.scatter(xt3_plot[0, :], xt3_plot[1, :], c=yt3_plot.flatten(), cmap='coolwarm', alpha=0.7)
 plt.scatter(xt3_plot[0, :], xt3_plot[1, :], c=out_plot, cmap='coolwarm', edgecolor='black', s=40)
@@ -76,5 +65,15 @@ plt.ylabel('x2')
 plt.title('Predicted Class Grid')
 cbar = plt.colorbar()
 cbar.set_label('Predicted Class')
+# Plot decision boundary
+h = 0.02  # Step size for the mesh grid
+x_min, x_max = xt3_plot[0, :].min() - 1, xt3_plot[0, :].max() + 1
+y_min, y_max = xt3_plot[1, :].min() - 1, xt3_plot[1, :].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+grid_points = np.c_[xx.ravel(), yy.ravel()]  # Flatten the mesh grid points
+Z = np.array(net.predict(grid_points)).flatten()  # Flatten the predicted classes
+Z = Z.reshape(xx.shape)
+plt.contour(xx, yy, Z, levels=[0], colors='black', linestyles='dashed')
+
 plt.tight_layout()
 plt.show()
